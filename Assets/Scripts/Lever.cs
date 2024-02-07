74245e166755a778fd5ce4a30de410c391d97932
Jusoboy <polygonalcube@protonmail.com>
2024-02-06 22:09:40 -0500

    Wind & lever interaction

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform pull;
+    public bool isOn = false;
+    public float rotAmt = 0f;
+    public float rotMulti = 0.1f;
+
     void Start()
     {
-        
+        pull.eulerAngles = new Vector3(-45f, pull.eulerAngles.y, pull.eulerAngles.z);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isOn)
+        {
+            float prevRot = pull.eulerAngles.x;
+            if (rotAmt < 90f)
+            {
+                pull.eulerAngles -= new Vector3(1f, 0f, 0f) * rotMulti;
+            }
+            rotAmt += Mathf.Abs(pull.eulerAngles.x - prevRot);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Air")
+        {
+            isOn = true;
+        }
     }
 }
