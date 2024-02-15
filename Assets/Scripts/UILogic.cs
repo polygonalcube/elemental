82fcd56657149795df35b76490611c44b6a9e409
Jusoboy <polygonalcube@protonmail.com>
2024-02-15 10:44:20 -0500

    UI & element display

--- Assets/Scripts/UILogic.cs
+++ Assets/Scripts/UILogic.cs
@@ -10,9 +10,26 @@ public class UILogic : MonoBehaviour
     [SerializeField] TextMeshProUGUI tutor;
     [SerializeField] float tutorDuration = 7f;
 
+    [SerializeField] Image earth;
+    [SerializeField] Image fire;
+    [SerializeField] Image air;
+    [SerializeField] Image sel;
+    PlayerLogic player;
+
     void Start()
     {
         StartCoroutine(TutorialDisplay());
+        player = GameManager.gm.FindPlayerScript();
+    }
+
+    void Update()
+    {
+        earth.gameObject.SetActive(player.unlocks[0]);
+        fire.gameObject.SetActive(player.unlocks[1]);
+        air.gameObject.SetActive(player.unlocks[2]);
+
+        //method for moving UI elements; if this yields unexpected results, replace "localPosition" with "anchoredPosition"
+        sel.GetComponent<RectTransform>().localPosition = new Vector3(650f + (120f * (int)player.element), -470f, 0f);
     }
 
     IEnumerator TutorialDisplay()
