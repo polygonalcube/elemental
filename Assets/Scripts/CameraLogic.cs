ee0455a5788e09bb23694a51b3c5582069102334
Jusoboy <polygonalcube@protonmail.com>
2024-02-27 20:20:49 -0500

    Beginning of documentation

--- Assets/Scripts/CameraLogic.cs
+++ Assets/Scripts/CameraLogic.cs
@@ -1,19 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraLogic : MonoBehaviour
 {
-    [SerializeField] float sensitivity = 3f;
+    // The script for the first-person camera.
+
+    [SerializeField] float sensitivity = 3f; // Higher values equate to stronger camera rotations.
 
     //mouse
-    public InputAction cam;
+    public InputAction cam; // Mouse input.
 
-    public Vector2 movValue = Vector2.zero;
-    float xRot = 0f;
-    Transform player;
+    public Vector2 movValue = Vector2.zero; // The variable that the result of the mouse input will be passed to.
+    float xRot = 0f; // The vertical rotation of the camera. Used to make sure players can't look past the top & bottom.
+    Transform player; // A reference to the player transform.
 
+    // Required for the input system to function.
     void OnEnable()
     {
         cam.Enable();
@@ -27,23 +30,23 @@ public class CameraLogic : MonoBehaviour
     void Start()
     {
         //you can quit play mode with Ctrl+P
-        Cursor.lockState = CursorLockMode.Locked;
-        player = GameManager.gm.FindPlayer().transform;
+        Cursor.lockState = CursorLockMode.Locked; // When you click on the game screen, the game will capture your mouse to allow for infinite panning.
+        player = GameManager.gm.FindPlayer().transform; // Sets the player variable to the player transform.
     }
 
     void Update()
     {
-        movValue = cam.ReadValue<Vector2>();
-        xRot -= movValue.y * sensitivity * Time.deltaTime;
-        xRot = Mathf.Clamp(xRot, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
-        if (player == null)
+        movValue = cam.ReadValue<Vector2>(); // The results of the frame's input gets passed to a variable.
+        xRot -= movValue.y * sensitivity * Time.deltaTime; // xRot gets decremented (dec because of Unity rotations) by the y component of input * sensitivity.
+        xRot = Mathf.Clamp(xRot, -90f, 90f); // Clamps the xRot variable. This prevents players from looking too far up or down.
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f); // Rotates the camera vertically.
+        if (player == null) // If the player variable is null...
         {
-            player = GameManager.gm.FindPlayer().transform;
+            player = GameManager.gm.FindPlayer().transform; // search for the player transform again. This will run each frame until the player is found.
         }
-        else
+        else // If not null...
         {
-            player.Rotate(Vector3.up, movValue.x * sensitivity * Time.deltaTime);
+            player.Rotate(Vector3.up, movValue.x * sensitivity * Time.deltaTime); // rotate the player horizontally.
         }
     }
 }
--- Assets/Scripts/ElementCollect.cs
+++ Assets/Scripts/ElementCollect.cs
@@ -1,22 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
 
 public class ElementCollect : MonoBehaviour
 {
+    // The logic for the permanent element power-ups.
+
+    //                     earth, fire,  air
     public bool[] which = {false, false, true};
 
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
-            PlayerLogic player = col.gameObject.GetComponent<PlayerLogic>();
+            PlayerLogic player = GameManager.gm.FindPlayerScript();
             for (int i = 0; i < player.unlocks.Length; i++)
             {
-                if (which[i])
-                {
-                    player.unlocks[i] = which[i];
-                }
+                if (which[i]) player.unlocks[i] = which[i];
             }
             Destroy(this.gameObject);
         }
